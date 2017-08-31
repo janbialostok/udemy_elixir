eefe1ee22f0db1f56ee16fd66ec773c2372890ce
Jan Bialostok <janbialostok@gmail.com>
2017-08-30 21:38:35 -0400

    continued coursework up to lecture 15

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,11 +1,26 @@
 defmodule Cards do
   
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    # naive solution for generating the cards non-optimal because uses intermediate computation
+    # cards = for suit <- suits do
+      # for value <- values do
+        # "#{ value } of #{ suit }"
+      # end
+    # end
+    # List.flatten(cards)
+    for suit <- suits, value <- values do
+      "#{ value } of #{ suit }"
+    end
   end
 
   def shuffle(deck) do 
-    
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 
   def save do 
