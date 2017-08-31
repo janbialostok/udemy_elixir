270285ea85b54252d1ff447ed5a6c1ede17d64f3
Jan Bialostok <janbialostok@gmail.com>
2017-08-30 20:39:05 -0400

    init udemy elixir repo

---
+++ cards/lib/cards.ex
@@ -0,0 +1,19 @@
+defmodule Cards do
+  
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do 
+    
+  end
+
+  def save do 
+    
+  end
+
+  def load do
+    
+  end
+
+end
