ea59ee153ffe978f357446ab964c5d263240cdc9
Jan Bialostok <janbialostok@gmail.com>
2017-09-02 08:24:34 -0400

    Maps @ lecture 32

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,11 @@
 defmodule Cards do
-  
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -19,11 +25,33 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
   # use pattern matching to retrieve values from returned tuple
   # { hand, deck } = deal(Cards.create_deck, 5)
+
+  @doc """
+    Divides of deck into a hand and the remainder of the deck. The `size` argument indicates how many cards should be in the hand.
+  
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> { hand, deck } = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+      
+  """
   def deal(deck, size) do
     Enum.split(deck, size)
   end
@@ -55,4 +83,16 @@ defmodule Cards do
   end
 
 end
+
+# definition of a map. Eqivalent of a regular typed JS object
+# colors = %{ primary: "red", secondary: "blue" }
+# there is also the ability to create an empty map functionally
+# colors = Map.new
+# pattern matching for assignments using maps
+# %{ secondary: secondary_color } = colors
+# updating a map can only be done functionally (property keys should always be atoms)
+# colors = Map.put(colors, :primary, "blue")
+# all updates are functional but there is a typed expression that performs an update
+# colors = %{ colors | primary: "blue" }
+# typed updates can't add new keys for the Map.put must be used
 
