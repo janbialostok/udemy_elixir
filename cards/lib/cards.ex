071eb355786d462979eddd9023edbe80854547f1
Jan Bialostok <janbialostok@gmail.com>
2017-09-01 22:44:33 -0400

    up to section 3 testing and documentation

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -22,13 +22,37 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
-
-  def save do 
-    
+  # use pattern matching to retrieve values from returned tuple
+  # { hand, deck } = deal(Cards.create_deck, 5)
+  def deal(deck, size) do
+    Enum.split(deck, size)
   end
 
-  def load do
-    
+  def save(deck, filename) do 
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    # you can multi-line case statements
+    case File.read(filename) do
+      # case statements will return a value allowing you to execute pattern matching assignments. Additionally match errors will be treated as falsy statements
+      { :ok, binary } -> :erlang.binary_to_term(binary)
+      # _ preceeding a variable declaration denotes a variable declaration that will not be used
+      { :error, _reason } -> "That file does not exist"
+    end
+  end
+
+  def create_hand(size) do
+    # naive code not using pipe operator
+    # deck = Cards.create_deck
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck, size)
+    # pipe operator injects return value from subsequent evaluation as first argument in preceeding function
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(size)
   end
 
 end
+
--- cards/mix.exs
+++ cards/mix.exs
@@ -21,6 +21,7 @@ defmodule Cards.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      { :ex_doc, "~> 0.12" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
@@ -28,3 +29,4 @@ defmodule Cards.Mixfile do
 end
 
 # use iex -S mix inside of a project directory to compile and run in the interactive shell
+# use mix deps.get to install Hex dependencies defined in defp deps do section
