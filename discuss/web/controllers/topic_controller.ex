81e336706d4856dda17acea9d0a5228a1a125ab3
Jan Bialostok <janbialostok@gmail.com>
2017-09-23 19:13:42 -0400

    added all rest methods

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -18,7 +18,10 @@ defmodule Discuss.TopicController do
   def create(conn, %{ "topic" => topic }) do
     # Repo.insert returns a tuple with either :ok or :error status and the posted value or invalid changeset respectively
     case Repo.insert(Topic.changeset(%Topic{}, topic)) do
-      { :ok, post } -> IO.inspect(post)
+      { :ok, _topic } ->
+        # put_flash method is exposed by the :controller and flashes a message of redirect similar to req.flash in express
+        put_flash(conn, :info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index)) # redirect method also exposed by :controller accepts conn and a argument with to: as a property which expects the topic_path method to be called with connection and routher path atom
       { :error, invalid } -> 
         render(conn, "new.html", changeset: invalid)
     end
@@ -27,5 +30,31 @@ defmodule Discuss.TopicController do
   def index(conn, _params) do
     topics = Repo.all(Topic)
     render(conn, "index.html", topics: topics)
+  end
+  
+  def edit(conn, %{ "id" => topic_id }) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{ "topic" => topic, "id" => topic_id }) do
+    original = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(original, topic)
+    case Repo.update(changeset) do
+      { :ok, _topic } ->
+        put_flash(conn, :info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      { :error, invalid } -> 
+        render(conn, "edit.html", changeset: invalid, topic: original)
+    end
+  end
+
+  def delete(conn, %{ "id" => topic_id }) do
+    # delete! (almost all the crud operations have ! methods) will specifically redirect the user to an error page vs delete which lets you explicitly specify error behaviour
+    Repo.get!(Topic, topic_id)
+    |> Repo.delete!
+    put_flash(conn, :info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
   end
 end
--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -15,10 +15,14 @@ defmodule Discuss.Router do
 
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
-
+    # this still needs to be defined even with resources defined because it slightly breaks RESTful conventions
     get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    # get "/topics/new", TopicController, :new
+    # get "/topics/:id/edit", TopicController, :edit
+    # post "/topics", TopicController, :create
+    # put "/topics/:id", TopicController, :update
+    # the below line mimics the behaviour above so long as the RESTful design the followed exactly like above
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
